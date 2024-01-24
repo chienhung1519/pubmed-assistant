72c02ca586d15d1d82df09aa3aea017b42dd05ff
chienhung1519 <chienhung1519@gmail.com>
2024-01-24 15:26:50 +0000

    debug

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,7 +74,8 @@ if prompt := st.chat_input("Message Pubmed Assistant"):
         st.markdown(reference(articles))
     st.session_state.messages.append({"role": "assistant", "content": f"{full_response}\n{reference(articles)})"})
 
-    data = conn.read(usecols=[0, 1, 2, 3], colums=["time", "user", "response", "reference"])
+    data = conn.read(usecols=[0, 1, 2, 3])
+    data.colums = ["time", "user", "response", "reference"]
     st.write(data)
     new_data = pd.DataFrame({"time": [datetime.datetime.now()], "user": [prompt], "response": [full_response], "reference": [reference(articles)]})
     st.write(new_data)
